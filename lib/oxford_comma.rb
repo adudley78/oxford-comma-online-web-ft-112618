1864ae5650af6eb607f168cfe4dd515e2f5a0b35
Adam Dudley <dudleyadam@gmail.com>
2018-11-26 19:22:04 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
     return "#{array[0]} and #{array[1]}"
   elsif 2 < array.length
       #le  binding.pry
-    array[-1].insert(0, "and")
+    array[-1].insert(0, "and ")
 
   end
   array.join(", ")
