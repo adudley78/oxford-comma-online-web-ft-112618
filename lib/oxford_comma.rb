dfb692c126af6ea11cf9bebeebe656510cf448a5
Adam Dudley <dudleyadam@gmail.com>
2018-11-26 19:21:42 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,8 +4,9 @@ def oxford_comma(array)
   if array.length == 2
     return "#{array[0]} and #{array[1]}"
   elsif 2 < array.length
-      #  binding.pry
+      #le  binding.pry
     array[-1].insert(0, "and")
 
   end
+  array.join(", ")
 end
