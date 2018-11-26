2aba3d3483749b70c7feeb646da90ea00a7e2bdb
Adam Dudley <dudleyadam@gmail.com>
2018-11-26 19:18:34 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def oxford_comma(array)
   if array.length == 2
-    array.join(" and ")
+    return "#{array[0]} and #{array[1]}"
   elsif 2 < array.length
     array[-1].insert(0, "and")
     binding.pry
